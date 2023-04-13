9519fe04a7506202ea6ccc21f03b4186d935be19
PavlosMak <pavlosmakridis1322001@gmail.com>
2023-04-13 17:03:57 +0200

    Improve thread detection with multiple program iterators

--- fence_insertion/insertion.py
+++ fence_insertion/insertion.py
@@ -1,8 +1,17 @@
 from fence_insertion.aeg import AbstractEventGraph
 
 class FenceInserter:
-    def __init__(self, aeg: AbstractEventGraph):
+    def __init__(self, aeg: AbstractEventGraph, program: list):
         self.aeg = aeg
+        self.program = program
 
+    def get_fences(self) -> list:
+        return list(10, 20)
     def insert_fences(self):
-        pass
+        # TODO: Replace this with actual
+        lines = self.get_fences() # The line numbers to insert full fences
+        acc = 0
+        for i in range(len(lines)):
+            self.program.insert(lines[i]+acc, "fence")
+            acc += 1
+
